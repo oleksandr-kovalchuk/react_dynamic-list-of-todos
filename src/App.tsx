b99f4c3f65e2686aa1d1fdab0ed770da1d991cc3
Oleksandr Kovalchuk <kov280396@gmail.com>
2025-01-29 17:33:12 +0200

    React dynamic list of TODOs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [allTodos, setAllTodos] = useState<Todo[]>([]);
+  const [filter, setFilter] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    getTodos()
+      .then(setAllTodos)
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const filteredTodos = useMemo(() => {
+    return allTodos.filter(todo => {
+      if (filter === 'active' && todo.completed) {
+        return false;
+      }
+
+      if (filter === 'completed' && !todo.completed) {
+        return false;
+      }
+
+      return todo.title.toLowerCase().includes(searchQuery.toLowerCase());
+    });
+  }, [filter, allTodos, searchQuery]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +43,18 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                currentFilter={filter}
+                setFilter={setFilter}
+                query={searchQuery}
+                setQuery={setSearchQuery}
+              />
             </div>
 
-            <div className="block">
-              <Loader />
-              <TodoList />
-            </div>
+            {isLoading ? <Loader /> : <TodoList todos={filteredTodos} />}
           </div>
         </div>
       </div>
-
-      <TodoModal />
     </>
   );
 };
